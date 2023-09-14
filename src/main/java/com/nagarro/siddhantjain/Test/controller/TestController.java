59eeb6c67e1c2076c431547a0b8a4534cdba6401
siddhantjain <siddhant.jain@nagarro.com>
2023-09-14 16:33:37 +0530

    Updated controller

--- src/main/java/com/nagarro/siddhantjain/Test/controller/TestController.java
+++ src/main/java/com/nagarro/siddhantjain/Test/controller/TestController.java
@@ -12,11 +12,6 @@ public class TestController {
 		return "This is a test API";
 	}
 	
-	@GetMapping("/test2")
-	public String test2() {
-		
-		return "This is a test2 API";
-	}
 
 	@GetMapping("/user")
 	public String user() {
